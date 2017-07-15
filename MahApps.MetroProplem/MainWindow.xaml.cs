87a9c4e336bfcf3c427c484b9d347b3cf94bdc78
DH <freedoumanian@gmail.com>
2017-07-15 20:25:32 +0300

    init commit

---
+++ MahApps.MetroProplem/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.MetroProplem.Annotations;
+
+namespace MahApps.MetroProplem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _firstName;
+
+        public MainWindow()
+        {
+          // values are ready before attaching the data context
+            InitializeComponent();
+            FirstName = "TEST";
+            DataContext = this;
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (value == _firstName) return;
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MahApps.MetroProplem/TestUserControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MahApps.MetroProplem
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl, IDataErrorInfo
+    {
+        public static readonly DependencyProperty FirstNameProperty = DependencyProperty.Register("FirstName",
+            typeof(string), typeof(UserControl1), new PropertyMetadata(default(string)));
+        public string FirstName
+        {
+            get { return (string)GetValue(FirstNameProperty); }
+            set { SetValue(FirstNameProperty, value); }
+        }
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public string this[string columnName]
+        {
+            // this break point breaks twice, the last time it returns null and the validation 
+            // read box doesn't disappear
+            get
+            {
+                return string.IsNullOrWhiteSpace(FirstName) ? "ERROR" : null;
+            }
+        }
+
+        public string Error { get; private set; }
+    }
+}
